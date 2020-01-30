e8d09463547804d88b368f7646df119a356d17b8
HFLaptop <haz.faizul@gmail.com>
2020-01-30 16:05:30 +0800

    First commit of React Calculator, no logic yet

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import KeyPad from "./components/KeyPad";
+import CalculatorDisplay from "./components/CalculatorDisplay";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100%" }}>
+        <div style={{
+          width: "350px",
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+        >
+          <CalculatorDisplay />
+          <KeyPad />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
